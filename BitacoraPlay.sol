15b950e9da483ca9182b107f2b6ab753c01b2abc
William <76260950+Willylllopiz@users.noreply.github.com>
2021-06-11 00:37:58 -0400

    Constructor and Register

---
+++ BitacoraPlay.sol
@@ -0,0 +1,81 @@
+pragma solidity ^0.5.10;
+
+contract BitacoraPlay{
+    event SignUpEvent(address indexed _newUser, uint indexed _userId, address indexed _sponsor, uint _sponsorId);
+    
+    ennum Range{
+        Rookie,
+        Junior,
+        Leader,
+        Guru
+    }
+    
+    struct User {
+        uint id;
+        address referrer;
+        Range range;
+        uint activeMembers;
+        address[] referrals;
+        
+    }
+    
+    uint public lastUserId = 2;
+    mapping(address => User) public users;
+    mapping(uint => address) public idToAddress;
+    
+    address public owner;
+    address externalAddress;
+    address rootAddress;
+    
+    modifier restricted() {
+        require(msg.sender == owner, "restricted");
+        _;
+    }
+    
+    constructor(address _externalAddress, address _rootAddress) public {
+        owner = msg.sender;
+        externalAddress = _externalAddress;
+        rootAddress = _rootAddress;
+        // initializeValues();
+
+        users[rootAddress].id = 1;
+        users[rootAddress].referrer = address(0);
+        idToAddress[1] = rootAddress;
+    }
+    
+     function registration(address userAddress, address referrerAddress) private {
+        require(!isUserExists(userAddress), "user exists");
+        require(isUserExists(referrerAddress), "referrer not exists");
+
+        uint32 size;
+        assembly {
+            size := extcodesize(userAddress)
+        }
+        require(size == 0, "cannot be a contract");
+        
+        idToAddress[lastUserId] = userAddress;
+        users[userAddress].id = lastUserId;
+        users[userAddress].referrer = referrerAddress;
+        
+        lastUserId++;
+        
+        user[referrerAddress].referrals = userAddress;
+        user[referrerAddress].activeMembers +=1;
+        
+        emit SignUpEvent(userAddress, users[userAddress].id, referrerAddress, users[referrerAddress].id);
+        // repartir ganancias!!!!!!!!!!!!!
+        
+     }
+     
+    function signUp(address referrerAddress) external payable {
+        require(msg.value == 5000000000000, "invalid registration cost");
+        registration(msg.sender, referrerAddress);
+    }
+    
+    function updateActivemembers(uint _level, address _referrerAddress){
+        if(level > 0 && _referrerAddress != rootAddress )
+    }
+
+}
+
+
